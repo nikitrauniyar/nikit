9223c8669fcd309796248397f3d1691320a59578
Nikit Rauniyar <rauniyarnikit@gmail.com>
2023-01-31 13:57:42 +1100

    format files

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,24 +1,24 @@
-import React, {useContext} from "react";
-import "./Footer.scss";
-import {Fade} from "react-reveal";
-import emoji from "react-easy-emoji";
-import StyleContext from "../../contexts/StyleContext";
+import React, { useContext } from 'react';
+import './Footer.scss';
+import { Fade } from 'react-reveal';
+import emoji from 'react-easy-emoji';
+import StyleContext from '../../contexts/StyleContext';
 
 export default function Footer() {
-  const {isDark} = useContext(StyleContext);
-  return (
-    <Fade bottom duration={1000} distance="5px">
-      <div className="footer-div">
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Copyright © Nikit Rauniyar, {new Date().getFullYear()}
-        </p>
-        {/* <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+	const { isDark } = useContext(StyleContext);
+	return (
+		<Fade bottom duration={1000} distance="5px">
+			<div className="footer-div">
+				<p className={isDark ? 'dark-mode footer-text' : 'footer-text'}>
+					Copyright © Nikit Rauniyar, {new Date().getFullYear()}
+				</p>
+				{/* <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
           <a href="https://github.com/saadpasta/developerFolio">
             developerFolio
           </a>
         </p> */}
-      </div>
-    </Fade>
-  );
+			</div>
+		</Fade>
+	);
 }
